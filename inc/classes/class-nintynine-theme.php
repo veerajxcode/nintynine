69936c17d8ccf956bcf605b40c6a6316da0fc41b
veerajxcode <veeraj11yadav@gmail.com>
2024-07-11 18:47:11 +0530

    dynamically adding menus to theme

--- inc/classes/class-nintynine-theme.php
+++ inc/classes/class-nintynine-theme.php
@@ -16,6 +16,7 @@ class NINTYNINE_THEME {
     protected function __construct(){
         //load class
         Assets::get_instance();
+        Menus::get_instance();
 
         $this->setup_hooks();
     }
@@ -28,6 +29,9 @@ class NINTYNINE_THEME {
     }
 
     public function setup_theme() {
+
+
+
         add_theme_support( 'title-tag' );
 
         add_theme_support( 'custom-logo', [
@@ -42,6 +46,35 @@ class NINTYNINE_THEME {
             'default-color' => '#fff',
             'default-image' => '',
         ] );
+
+        add_theme_support( 'post-thumbnails' );
+
+        add_theme_support( 'customize-selective-refresh-widgets' );
+
+        add_theme_support( 'automatic-feed-links' );
+
+        add_theme_support( 
+            'html5',
+            [
+                'search-form',
+                'comment-form',
+                'comment-list',
+                'gallery',
+                'caption',
+                'script',
+                'style',
+            ] 
+        );
+
+        add_editor_style();
+        add_theme_support( 'wp-block-styles' );
+        add_theme_support( 'align-wide' );
+
+        global $content_width;
+        if( ! isset( $content_width ) ) {
+            $content_width = 1240;
+        }
+
     }
     
 }
