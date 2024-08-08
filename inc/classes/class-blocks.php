350e0d5c7c58752fc028661e6315947cfe1565fe
veerajxcode <veeraj11yadav@gmail.com>
2024-08-08 12:19:51 +0530

    commiting remaining files

---
+++ inc/classes/class-blocks.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Theme Blocks
+ * 
+ * @package NintyNine
+ */
+
+namespace NINTYNINE_THEME\Inc;
+
+use NINTYNINE_THEME\Inc\Traits\Singleton;
+
+class Blocks {
+    use Singleton;
+
+    protected function __construct(){
+        //load class.
+        $this->setup_hooks();
+    }
+
+    protected function setup_hooks(){
+        /**
+         * Actions
+         */
+        add_action( 'block_categories', [ $this, 'add_block_categories' ] );
+    }
+
+    public function add_block_categories( $categories ) {
+        $categories_slugs = wp_list_pluck( $categories, 'slug' );
+
+        return  in_array( 'nintynine', $categories_slugs, true ) ? $categories : array_merge( $categories, 
+            [
+                [ 
+                    'slug' => 'nintynine', 
+                    'title' => __( 'NintyNine Blocks', 'nintynine' ), 
+                    'icon' => 'table-row-after' 
+                ]
+            ]
+        );
+    }
+   
+}
